9f3b5616a64ac3a6d2506f9935d57db6253342b1
wnicolass <nicolaswgl99@gmail.com>
2023-02-17 02:27:53 +0000

    feat: generate jwt token for user

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import './src/database';
 import express from 'express';
 import homeRoutes from './src/routes/home';
 import userRoutes from './src/routes/user';
+import tokenRoutes from './src/routes/token';
 
 class App {
   constructor() {
@@ -22,6 +23,7 @@ class App {
   routes() {
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokenRoutes);
   }
 }
 
