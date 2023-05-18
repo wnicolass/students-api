dc0261e30c72d8e63312cc8491a1c9ec88da3045
wnicolass <nicolaswgl99@gmail.com>
2023-05-18 22:04:23 +0100

    build: add cors middleware

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,6 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _path = require('path');
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 
 _dotenv2.default.config();
 
@@ -11,6 +12,20 @@ var _token = require('./routes/token'); var _token2 = _interopRequireDefault(_to
 var _student = require('./routes/student'); var _student2 = _interopRequireDefault(_student);
 var _file = require('./routes/file'); var _file2 = _interopRequireDefault(_file);
 
+const whiteList = [
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin: (origin, cb) => {
+    if (!origin || whiteList.includes(origin)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 class App {
   constructor() {
     this.app = _express2.default.call(void 0, );
@@ -19,6 +34,7 @@ class App {
   }
 
   middlewares() {
+    this.app.use(_cors2.default.call(void 0, corsOptions));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
--- dist/routes/user.js
+++ dist/routes/user.js
@@ -8,7 +8,7 @@ const router = new (0, _express.Router)();
 router.get('/', _UserController2.default.index); // list all users
 router.get('/:id', _UserController2.default.show); // list one user
 
-router.post('/', _loginRequired2.default, _UserController2.default.store);
+router.post('/', _UserController2.default.store);
 router.put('/', _loginRequired2.default, _UserController2.default.update);
 router.delete('/', _loginRequired2.default, _UserController2.default.delete);
 
